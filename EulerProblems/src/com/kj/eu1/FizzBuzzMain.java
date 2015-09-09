222a048d24d1635503218d0e13e107486703ecdc
KunalJoshi1 <kunaljoshi8@gmail.com>
2015-09-09 15:14:56 -0700

    Gitignore file and first commit

---
+++ EulerProblems/src/com/kj/eu1/FizzBuzzMain.java
@@ -0,0 +1,14 @@
+package com.kj.eu1;
+
+public class FizzBuzzMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is solution to FizzBuzz problem, Euler1");
+
+	}
+
+}
